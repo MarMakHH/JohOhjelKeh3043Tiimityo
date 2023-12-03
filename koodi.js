bb9321b9a83782d034b981026f625f5afe120d59
bhi021@myy.haaga-helia.fi <bhi021@myy.haaga-helia.fi>
2023-12-03 16:39:45 +0200

    uusi sanan tarkistus logiikka

--- koodi.js
+++ koodi.js
@@ -40,19 +40,24 @@ function arvoSana(vaikeus) {
 //Kun pelaaja tekee arvauksen --> aktivoi
 function arvaa() {
     let arvaus = String(document.getElementById("inputw").value);
-    arvaus = muokkaaSana(arvaus);
-    let arvausL = teeLista(arvaus);
-    let arvattavaL = teeLista(arvattava);
-    console.log(arvausL);
-    tulostaArvaus(arvausL, arvattavaL);
-    arvauksiakpl--;
-    document.getElementById("info").innerHTML = "Word length: " + arvattava.length + "<br>Guesses left: " + arvauksiakpl;
-    if (vertaaSana(arvattava, arvaus)) {
-        document.getElementById("pelialue").innerHTML = "<div class='won'> YOU WON! </div>";
-    } else if (arvauksiakpl == 0) {
-        document.getElementById("pelialue").innerHTML = "<div class='lost'> YOU LOST! </div> <br>The world was: " + arvattava;
-    }
     document.getElementById("inputw").value = "";
+    arvaus = muokkaaSana(arvaus);
+    if (arvaus.length == arvattava.length) {
+        let arvausL = teeLista(arvaus);
+        let arvattavaL = teeLista(arvattava);
+        console.log(arvausL);
+        tulostaArvausv2(arvausL, arvattavaL);
+        arvauksiakpl--;
+        document.getElementById("info").innerHTML = "Word length: " + arvattava.length + "<br>Guesses left: " + arvauksiakpl;
+        if (vertaaSana(arvattava, arvaus)) {
+            document.getElementById("pelialue").innerHTML = "<div class='won'> YOU WON! </div>";
+        } else if (arvauksiakpl == 0) {
+            document.getElementById("pelialue").innerHTML = "<div class='lost'> YOU LOST! </div> <br>The world was: " + arvattava;
+        }
+
+    }
+    
+
 }
 
 //siistii sanan, jotta se on samassa muodossa kuin wordlist.js
@@ -97,6 +102,14 @@ function teeLista(sana) {
     return lista;
 }
 
+function teeString(lista) {
+    let sana = lista.toString();
+    for (let i = 0; i < sana.length; i++) {
+        sana = sana.replace(",", "");
+    }
+    return sana;
+}
+
 //luo taulukon ja vertaa kirjaimet ja/tai sanat. Class kertoo vertauksen tuloksen 
 function tulostaArvaus(arvausSana, arvattavaSana) {
     let vastaus = "<tr>";
@@ -105,6 +118,39 @@ function tulostaArvaus(arvausSana, arvattavaSana) {
             vastaus += "<th class='oikein'>" + arvausSana[i] + "</th>";
         } else if (vertaaSanaJaKirjain(arvattava, arvausSana[i])) {
             vastaus += "<th class='melkein'>" + arvausSana[i] + "</th>";
+        } else {
+            vastaus += "<th class='eiok'>" + arvausSana[i] + "</th>";
+        }
+    }
+    vastaus += "</tr>"
+    document.getElementById("arvaukset").innerHTML += vastaus;
+}
+
+function tulostaArvausv2(arvausSana, arvattavaSana) {
+    let vastausL = [];
+    let vertailuArvattava = teeString(arvattavaSana);
+    let vastaus = "<tr>";
+    for (let i = 0; i < arvausSana.length; i++) {
+        if (vertaaKirjain(arvausSana[i], arvattavaSana[i])) {
+            vastausL.push(2);
+            vertailuArvattava = vertailuArvattava.replace(arvausSana[i], "");
+        } else if (vertaaSanaJaKirjain(arvattava, arvausSana[i])) {
+            vastausL.push(1);
+        } else {
+            vastausL.push(0);
+        }
+    }
+    for (let i = 0; i < arvausSana.length; i++) {
+        if (vastausL[i] == 2) {
+            vastaus += "<th class='oikein'>" + arvausSana[i] + "</th>";
+        } else if (vastausL[i] == 1) {
+            if (vertaaSanaJaKirjain(vertailuArvattava, arvausSana[i])) {
+                vastaus += "<th class='melkein'>" + arvausSana[i] + "</th>";
+                vertailuArvattava = vertailuArvattava.replace(arvausSana[i], "");
+            } else {
+                vastaus += "<th class='eiok'>" + arvausSana[i] + "</th>";
+            }
+
         } else {
             vastaus += "<th class='eiok'>" + arvausSana[i] + "</th>";
         }
